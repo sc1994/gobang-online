84b6a030e7d75eccf49c7c3f48cdede38b17687c
孙诚 <sunchengsuncheng@gmail.com>
2019-03-19 13:38:28 +0800

    fix

--- SocketAPI/Startup.cs
+++ SocketAPI/Startup.cs
@@ -30,13 +30,14 @@ namespace SocketAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("SignalR");         // 启用跨域
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UsePathBase("/service");    // 启用二级目录
-            app.UseCors("SignalR");         // 启用跨域
             app.UseStaticFiles("/wwwroot"); // 静态文件服务
             app.UseHttpsRedirection();      // 启用http上下文
 
