5453a1c60d3749e2a919a9bc9f7a795549265396
孙诚 <sunchengsuncheng@gmail.com>
2019-03-19 11:18:34 +0800

    sokcet

---
+++ SocketAPI/Controllers/SendController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SocketAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SendController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(string token, string msg)
+        {
+            return new[] { token, msg };
+        }
+    }
+}
